8bc4ea7620a435c943c2b04e79b7961574618a6f
Mustafa Ghanem <mughanem@Mustafas-MacBook-Pro.local>
2023-11-05 01:40:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 49.005318370152615, lng: 31.54402010470768};
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+initMap();
